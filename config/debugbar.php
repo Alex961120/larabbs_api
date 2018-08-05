a59a7ecef0b979fa63a4097d8d9615a06b7dce65
Alex961120 <805119233@qq.com>
2018-08-05 11:08:52 +0800

    fix laravel-debugbar

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,7 +14,8 @@ return [
      |
      */
 
-    'enabled' => env('APP_DEBUG', false),
+//    'enabled' => env('APP_DEBUG', false),
+    'enabled' => env('DEBUG_ENABLE', false),
 
     'except' => [
         //
